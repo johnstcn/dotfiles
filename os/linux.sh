728edc000ac79d852d0d4c7af4f5cf2566261659
Cian Johnston <public@cianjohnston.ie>
2026-02-06 23:15:24 +0000

    refactor(linux): skip Neovim installation if correct version is present

--- os/linux.sh
+++ os/linux.sh
@@ -7,9 +7,13 @@ if [[ "$NVIM_ARCH" == "aarch64" ]]; then
 fi
 NVIM_URL="https://github.com/neovim/neovim/releases/download/${NVIM_VERSION}/nvim-linux-${NVIM_ARCH}.tar.gz"
 
+INSTALL_NVIM=true
 if command -v nvim &>/dev/null; then
     SYSTEM_NVIM_VER=$(nvim --version | head -n1 | cut -d' ' -f2 | sed 's/^v//')
-    if [[ "$SYSTEM_NVIM_VER" == 0.9.* ]]; then
+    if [[ "$SYSTEM_NVIM_VER" == "${NVIM_VERSION#v}" ]]; then
+        log system "Neovim ${SYSTEM_NVIM_VER} already installed, skipping binary download"
+        INSTALL_NVIM=false
+    elif [[ "$SYSTEM_NVIM_VER" == 0.9.* ]]; then
         log system "WARNING: An outdated version of Neovim (v0.9.x) was detected in your system PATH."
         log system "         The new version will be installed to $HOME/bin/nvim and should take precedence."
     fi
@@ -32,6 +36,9 @@ DOTFILES_BINARIES=(
     "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubectx"
     "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubens_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubens"
     "https://github.com/justjanne/powerline-go/releases/download/v1.26/powerline-go-${OS}-${ARCH}|$HOME/bin/powerline-go"
-    "$NVIM_URL|$HOME/bin/nvim"
     "https://github.com/fluxcd/flux2/releases/download/v2.7.5/flux_2.7.5_linux_amd64.tar.gz|$HOME/bin/flux"
 )
+
+if [[ "$INSTALL_NVIM" == "true" ]]; then
+    DOTFILES_BINARIES+=("$NVIM_URL|$HOME/bin/nvim")
+fi
