fe956b910e0e58fd20f414f31d92208cefdf045d
Cian Johnston <public@cianjohnston.ie>
2026-02-06 23:02:46 +0000

    Update Neovim to 0.11.6 and move to user-local install in os/linux.sh

--- os/linux.sh
+++ os/linux.sh
@@ -1,5 +1,18 @@
 # shellcheck shell=bash
 # shellcheck disable=SC2034
+NVIM_ARCH=$(uname -m)
+if [[ "$NVIM_ARCH" == "aarch64" ]]; then
+    NVIM_ARCH="arm64"
+fi
+NVIM_URL="https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-${NVIM_ARCH}.tar.gz"
+
+if command -v nvim &>/dev/null; then
+    SYSTEM_NVIM_VER=$(nvim --version | head -n1 | cut -d' ' -f2 | sed 's/^v//')
+    if [[ "$SYSTEM_NVIM_VER" == 0.9.* ]]; then
+        log system "WARNING: Outdated system Neovim detected ($SYSTEM_NVIM_VER). Configuration requires v0.10+."
+    fi
+fi
+
 DOTFILES_PACKAGES=(
     bat
     byobu
@@ -7,7 +20,6 @@ DOTFILES_PACKAGES=(
     fzf
     jq
     less
-    neovim
     shellcheck
     shfmt
     tree
@@ -18,6 +30,6 @@ DOTFILES_BINARIES=(
     "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubectx"
     "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubens_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubens"
     "https://github.com/justjanne/powerline-go/releases/download/v1.26/powerline-go-${OS}-${ARCH}|$HOME/bin/powerline-go"
-    "https://github.com/neovim/neovim/releases/download/v0.11.6/nvim-linux-x86_64.tar.gz|$HOME/bin/nvim"
+    "$NVIM_URL|$HOME/bin/nvim"
     "https://github.com/fluxcd/flux2/releases/download/v2.7.5/flux_2.7.5_linux_amd64.tar.gz|$HOME/bin/flux"
 )
