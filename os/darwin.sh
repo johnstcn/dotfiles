00bca2a3525f2550b9514756717099dee0e30253
Cian Johnston <public@cianjohnston.ie>
2026-02-06 15:57:37 +0000

    Add OS-specific configuration scripts for Linux and Darwin

---
+++ os/darwin.sh
@@ -0,0 +1,20 @@
+DOTFILES_PACKAGES=(
+    bat
+    byobu
+    fd
+    flux
+    fzf
+    go
+    kubectl
+    kubectx
+    jq
+    less
+    neovim
+    powerline-go
+    shellcheck
+    tree
+    vim
+)
+
+# On macOS, most things are handled via Homebrew packages.
+DOTFILES_BINARIES=()
---
+++ os/linux.sh
@@ -0,0 +1,20 @@
+DOTFILES_PACKAGES=(
+    bat
+    byobu
+    fd-find
+    flux
+    fzf
+    jq
+    less
+    neovim
+    shellcheck
+    tree
+    vim
+)
+
+DOTFILES_BINARIES=(
+    "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubectx"
+    "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubens_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubens"
+    "https://github.com/justjanne/powerline-go/releases/download/v1.26/powerline-go-${OS}-${ARCH}|$HOME/bin/powerline-go"
+    "https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-linux-x86_64.tar.gz|/opt/bin/nvim"
+)
