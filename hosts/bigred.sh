84b4672f5a4ff5792e186b1465031f9d53ae94d1
Cian Johnston <public@cianjohnston.ie>
2026-02-06 15:38:22 +0000

    Update bigred.sh with new binaries and remove redundant dotfiles

--- hosts/bigred.sh
+++ hosts/bigred.sh
@@ -20,17 +20,13 @@ DOTFILES_GIT_REPOS=(
     "git@gitlab.com:johnstcn/cv.git|$HOME/src/cv|master"
 )
 
-# Format: "url|dest"
+# Neovim uses different OS/ARCH naming
+local nvim_os="linux"
+local nvim_arch="x86_64"
+
 DOTFILES_BINARIES=(
     "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubectx_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubectx"
     "https://github.com/ahmetb/kubectx/releases/download/v0.9.5/kubens_v0.9.5_linux_x86_64.tar.gz|$HOME/bin/kubens"
-)
-
-# Format: "src|dest"
-DOTFILES_FILES=(
-    "bashrc|$HOME/.bashrc"
-    "bash_profile|$HOME/.bash_profile"
-    "bash_aliases|$HOME/.bash_aliases"
-    "gitconfig|$HOME/.gitconfig"
-    "vimrc|$HOME/.vimrc"
+    "https://github.com/justjanne/powerline-go/releases/download/v1.26/powerline-go-\${OS}-\${ARCH}|$HOME/bin/powerline-go"
+    "https://github.com/neovim/neovim/releases/download/v0.10.4/nvim-${nvim_os}-${nvim_arch}.tar.gz|/opt/bin/nvim"
 )
